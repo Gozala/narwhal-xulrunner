d7a63507df4785c3103f31165dbe58d57e2a49a9
Irakli Gozalishvili <rfobic@gmail.com>
2009-11-22 23:20:44 +0100

    Additional fixes to the websocket implementation some headers were required by chrome

--- examples/websocket.js
+++ examples/websocket.js
@@ -1,7 +1,7 @@
 ['Demo module for WebSocket server implementation\
 in order to test follow the links see link\
 @see http://github.com/gimite/web-socket-js\
-@example require("websocket-server").run(hello, { port: 10081 });']
+@example require("websocket-server").run(hello, { port: 7070 });']
 
 var hello = exports.hello = function(websocket) {
     ['Exameple websocket app. As client side relays\
@@ -17,7 +17,11 @@ var hello = exports.hello = function(websocket) {
         websocket.send("Hi " + event.data + "!");
    };
    require("websocket-server").FlashSocketPolicyHandler(websocket);
+};
+
+exports.main = function() {
+    print("starting websocket server");
+    require("websocket-server").run(hello, { port: 7070 });
 }
 
-if (require.main === module.id)
-    require("websocket-server").run(hello, { port: 10081 });
+if (require.main == module.id) exports.main();
